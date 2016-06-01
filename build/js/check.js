4f223778b437c14a5ef2eb97bfc6743b0d265247
dmpinevich <lokorash@gmail.com>
2016-06-01 21:24:15 +0300

    исправление ошибок

--- build/js/check.js
+++ build/js/check.js
@@ -1,37 +1,35 @@
 
 function getMessage(a,b) {
-  switch (typeof a) {
-    case 'boolean':
-      if (a) {
-        return 'Переданное GIF-изображение анимировано и содержит [' + b + '] кадров';
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    }
+    else
+    {
+      return 'Переданное GIF-изображение не анимировано';
+    }
+    
+  } else if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+  } else if (typeof a === 'object') {
+    if (Array.isArray(a)) {
+      if (Array.isArray(b)) {
+        for (var i = 0; i < a.length; i++) {
+          var square = 0;
+          square += a[i] * b[i];
+        }
+        return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
       }
       else {
-        return 'Переданное GIF-изображение не анимировано';
-      }
-    case 'number' :
-      return 'Переданное SVG-изображение содержит [' + a + '] объектов и [' + (b * 4) + '] атрибутов';
-
-    case 'object' :
-      if (Array.isArray(a)) {
-        if (Array.isArray(b)) {
-          for (i = 0; i < a.length; i++) {
-            square = 0;
-            square += a[i] * b[i];
-          }
-          return 'Общая площадь артефактов сжатия: [' + square + '] пикселей';
+        var sum = 0;
+        for (var i = 0; i < a.length; i++) {
+          sum += a[i];
         }
-        else {
-          var sum = 0;
-          for (i = 0; i < a.length; i++) {
-            sum += a[i];
-          }
 
-          return 'Количество красных точек во всех строчках изображения: [' + sum + ']';
-        }
+        return 'Количество красных точек во всех строчках изображения: ' + sum + '';
       }
-
+    }
   }
-
 
 
 }
