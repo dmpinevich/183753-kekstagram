25ec693ba3c67ce4ae7201265006df30755305ee
dmpinevich <lokorash@gmail.com>
2016-06-21 23:39:51 +0300

    module3-task2 валидация формы .Добавлены атрибуты disabled и enabled , а также обработчики событий oninput на resizeX , resizeY, resizeSize.Исправление #2

--- src/upload.js
+++ src/upload.js
@@ -76,13 +76,14 @@
     var resizeY = document.querySelector('#resize-y');
     var resizeSize = document.querySelector('#resize-size');
     var confButton = document.querySelector('#resize-fwd');
-    if ( !resizeX.value || !resizeY.value || (resizeX.value + resizeSize.value - currentResizer._image.naturalWidth) || (resizeY.value + resizeSize.value - currentResizer._image.naturalHeight)) {
+    if ( !resizeX.value || !resizeY.value || (resizeX.value + resizeSize.value - currentResizer._image.naturalWidth) || (resizeY.value + resizeSize.value - currentResizer._image.naturalHeight))
+      {
       confButton.disabled = true;
-    }
+      }
     else
-    {
+      {
       confButton.disabled = false;
-    }
+      }
     resizeX.oninput = function() {
       resizeFormIsValid();
     };
