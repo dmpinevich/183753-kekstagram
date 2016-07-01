af66342410d00bb319ac734ee0e3608ee0f91ae9
dmpinevich <lokorash@gmail.com>
2016-07-02 00:03:51 +0300

    module4-task1 #6

--- src/pictures.js
+++ src/pictures.js
@@ -1,29 +1,29 @@
 'use strict';
 var picturesContainer = document.querySelector('.pictures');
 document.forms[0].hidden = true;
-var templateElement = document.querySelector('template');
+var templateElement = document.querySelector('.template');
 var elementToClone;
 if ('content' in templateElement) {
-  elementToClone = templateElement.content.querySelector('picture');
+  elementToClone = templateElement.content.querySelector('.picture');
 } else {
-  elementToClone = templateElement.querySelector('picture');
+  elementToClone = templateElement.querySelector('.picture');
 }
 var getPictureElement;
 getPictureElement = function(data, container) {
   var element = elementToClone.cloneNode(true);
-  element.querySelector('picture-stats').textContent = data.date;
-  element.querySelector('picture-stat picture-comments').textContent = data.comments;
-  element.querySelector('picture-stat picture-likes').textContent = data.likes;
+  element.querySelector('.picture-stats').textContent = data.date;
+  element.querySelector('.picture-stat picture-comments').textContent = data.comments;
+  element.querySelector('.picture-stat picture-likes').textContent = data.likes;
   container.appendChild('element');
+  var contentImage = new Image();
   contentImage.onload = function(evt) {
-    element.firstChild.src = evt.target.url;
-    element.firstChild.width = 182;
-    element.firstChild.height = 182;
+    element.firstChild.src = "evt.target.url";
+    element.firstChild.width = "182px";
+    element.firstChild.height = "182px";
   };
   contentImage.onerror = function() {
-    element.classList.add('picture-load-failure');
+    element.classList.add('.picture-load-failure');
   };
-  var contentImage = new Image();
   contentImage.src = data.url;
   return element;
 };
